89383a1ff3e60fd6352f3f956935bcd639c87172
DenisKrigner <105937359+DenisKrigner@users.noreply.github.com>
2024-12-07 01:17:39 +0300

    Add files via upload

--- code.js
+++ code.js
@@ -8,7 +8,7 @@ let btn1 = document.getElementById("btt").addEventListener("click", function()
         {
             tg.MainButton.setText("Вы выбрали товар 5!");
             item = "6"
-            tg.sendData("")
+            tg.sendData(JSON.stringify(item))
             tg.close();
         });
 
